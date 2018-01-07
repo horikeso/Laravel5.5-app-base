cbfa66986f2c2dc32816cbb46ce80afaf6525b6c
horikeso <horiguchi.android@gmail.com>
2018-01-07 17:31:20 +0900

    mail setting

--- config/mail.php
+++ config/mail.php
@@ -16,6 +16,7 @@ return [
     |
     */
 
+    // 'driver' => env('MAIL_DRIVER', 'smtp'),
     'driver' => env('MAIL_DRIVER', 'sendmail'),
 
     /*
@@ -29,7 +30,8 @@ return [
     |
     */
 
-    'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
+    // 'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
+    'host' => env('MAIL_HOST', 'localhost'),
 
     /*
     |--------------------------------------------------------------------------
@@ -42,7 +44,8 @@ return [
     |
     */
 
-    'port' => env('MAIL_PORT', 587),
+    // 'port' => env('MAIL_PORT', 587),
+    'port' => env('MAIL_PORT', 25),
 
     /*
     |--------------------------------------------------------------------------
